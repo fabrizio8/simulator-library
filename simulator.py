33e1a93e872427cce4a3ca5e627aee16760128f4
Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>
2020-09-09 13:11:37 -0400

    task #3 WIP
    Can correctly generate some of nth strings correctly.
    Need to fix an error with low Ns
    
    Signed-off-by: Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>

--- simulator.py
+++ simulator.py
@@ -1,9 +1,16 @@
 from random import randint
+from itertools import product
+from math import log, ceil
 
 class Alphabet:
-    data = []
+    alphabet = []
     def __init__(self, *args):
         self.alphabet = [*args]
     def gen_rand_string(self, length):
         return [self.alphabet[c] for c in [randint(0,length-1) for _ in range(length)]] 
-
+    def get_nth_string(self, n):
+        if n == 0:
+            return list(product(self.alphabet, repeat=1))[0]
+        exp = ceil(log(n, len(self.alphabet)))
+        idx = n-sum(len(self.alphabet)**x for x in range(exp))
+        return list(product(self.alphabet, repeat=exp))[idx-1]
