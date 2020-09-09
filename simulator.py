6caaecd65cf53b19a06a7472fcb856f9f13b3f25
Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>
2020-09-09 19:31:10 -0400

    Completed Task #4
    DFAs represented as a class with member variables
    define the DFA
    
    Signed-off-by: Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>

--- simulator.py
+++ simulator.py
@@ -1,12 +1,32 @@
 from random import randint
+from typing import Callable
 
 class Alphabet:
     alphabet = []
     def __init__(self, *args):
         self.alphabet = [*args]
+
+
     def gen_rand_string(self, length):
         return [self.alphabet[c] for c in [randint(0,length-1) for _ in range(length)]] 
+
+
     def lexi(self, n):
         if n == 0:
             return []
         return self.lexi((n-1)//len(self.alphabet)) + [self.alphabet[(n-1)%len(self.alphabet)]]
+
+
+class DFA:
+    Q: Callable = None
+    sig: Alphabet = None
+    delt: Callable = None
+    q = None
+    F: Callable = None
+    
+    def __init__(Q,sig,delt,q,F):
+        self.Q = Q
+        self.sig = sig
+        self.delt = delt
+        self.q = q
+        self.F = F
--- tests/test_general.py
+++ tests/test_general.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
+
 import pytest
 from simulator import *
 
 def test_alphabet_init():
     a = Alphabet('A', 'B', 1,2,3)
     assert a.alphabet == ['A', 'B', 1, 2, 3]
+
+
 def test_alphabet_rand_string():
     a = Alphabet('A', 'B', 1,2,3)
     rand_string = a.gen_rand_string(5)
     assert len(rand_string) == 5
     assert all(x in a.alphabet for x in rand_string)
+
+
 def test_nth_string():
     a = Alphabet(1,2,3,4,5)
     assert a.lexi(0) == []
