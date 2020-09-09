e8e81833fe2c860a11435b7277b0e3f73a766bd0
Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>
2020-09-09 07:48:55 -0400

    Completed Task #2
    Strings are represented as lists.
    
    Signed-off-by: Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>

--- simulator.py
+++ simulator.py
@@ -1,4 +1,9 @@
+from random import randint
+
 class Alphabet:
     data = []
     def __init__(self, *args):
-        self.data = [*args]
+        self.alphabet = [*args]
+    def gen_rand_string(self, length):
+        return [self.alphabet[c] for c in [randint(0,length-1) for _ in range(length)]] 
+
--- tests.py
+++ tests.py
@@ -3,4 +3,8 @@
 from simulator import *
 
 a = Alphabet('A', 'B', 1,2,3)
-print(a.data)
+assert a.alphabet == ['A', 'B', 1, 2, 3]
+rand_string = a.gen_rand_string(5)
+assert len(rand_string) == 5
+print(rand_string)
+assert all(x in a.alphabet for x in rand_string)
