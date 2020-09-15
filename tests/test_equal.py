e408fe92fedcc7e8674b51f8ef375279e75149cc
Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>
2020-09-14 22:52:52 -0400

    Completed Task #22
    Signed-off-by: Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>

--- tests/test_equal.py
+++ tests/test_equal.py
@@ -2,6 +2,7 @@ from simulator import *
 from example_dfa import *
 from itertools import combinations
 
+f = gen_DFA_base_b_divisible_by_n
 def test_basic():
     for a,b in combinations(dfas,2):
         try:
@@ -10,9 +11,16 @@ def test_basic():
             if a is even_chars and b is strictly_alternating:
                 assert equal(a,b)
 
-def test_math():
-    f = gen_DFA_base_b_divisible_by_n
+def test_union():
     assert equal(f(10,2), f(10,2))
     assert equal(union(f(10,4),f(10,2)),union(f(10,2),f(10,4)))
+
+def test_intersect():
     for a,b in enumerate(range(1,10),2):
         equal(intersect(f(10, a), f(10,b)), f(10,a*b))
+
+def test_complement():
+    assert equal(f(10,2).complement().complement(), f(10,2))
+
+    for dfa in dfas:
+        assert equal(dfa.complement().complement(), dfa)
