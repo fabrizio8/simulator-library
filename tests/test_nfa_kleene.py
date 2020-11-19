4325509d1fab0a387ed4bebd65596f8d63d2720f
Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>
2020-11-18 23:22:20 -0500

    Almost forgot Kleene tests
    Signed-off-by: Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>

--- tests/test_nfa_kleene.py
+++ tests/test_nfa_kleene.py
@@ -1,5 +1,27 @@
 from simulator import *
 from example_nfa import *
 
-def test_general():
+def test_a():
     a = kleene_star(even_0_or_1)
+    assert a._accepted('')
+    assert a._accepted('1')
+    assert a._accepted('10')
+
+def test_b():
+    a = kleene_star(third_from_last_is_0)
+    assert a._accepted('111100011000')
+
+def test_c():
+    a = kleene_star(is_one)
+    assert a._accepted('1')
+    assert not a._accepted('0')
+    assert a._accepted('1111')
+
+def test_d():
+    a = concat_nfa(is_zero_or_one, is_zero_or_one)
+    assert a._accepted('11')
+    assert not a._accepted('111')
+    a = kleene_star(a)
+    assert a._accepted('11')
+    assert a._accepted('101010')
+    assert a._accepted('1010101111')
