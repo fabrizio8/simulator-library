d434059fffb0a3df3c6c98b3532211cb812a677d
Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>
2020-12-09 14:20:55 -0500

    More regex example and more cleanup
    Signed-off-by: Fabrizio D'Angelo <fabrizio_dangelo@student.uml.edu>

--- example_regex.py
+++ example_regex.py
@@ -13,6 +13,11 @@ zero_or_one = RE_union(one,zero)
 #01, 10
 any_amount_0_or_1 = RE_star(zero_or_one)
 #010101010101, 1011110010100000101, 0, 1, 10, 01
+alternating_10 = RE_star(RE_cirq(one,zero))
+# 10, 1010, 101010, 10101010
+alternating_10_any_amount_of_1 = RE_cirq(alternating_10, RE_star(one))
+#101010, 10101011111, 10111111
+zero_or_one_followed_by_111 = RE_concat(zero_or_one, RE_concat(RE_concat(one, one), one))
 
 re_list = [
         all_one,
@@ -20,4 +25,7 @@ re_list = [
         zero_followed_by_ones,
         zero_or_one,
         any_amount_0_or_1,
+        alternating_10,
+        alternating_10_any_amount_of_1,
+        zero_or_one_followed_by_111,
         ]
